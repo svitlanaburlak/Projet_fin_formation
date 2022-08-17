fc93c9dabf6a72e9670d1d4bbd26bb889f363d36
MarilyneDruart <marilyne.druart@outlook.com>
2022-08-17 14:53:28 +0200

    add Form CityType

---
+++ src/Form/CityType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\City;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CityType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('image')
+            ->add('country')
+            ->add('slug')
+            ->add('description')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => City::class,
+        ]);
+    }
+}
