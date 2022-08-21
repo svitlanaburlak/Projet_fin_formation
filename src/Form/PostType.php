e215966ce42273d01b9bb2adb22b7cec712bf451
svitlanaburlak <sfetlana.b@gmail.com>
2022-08-21 12:08:30 +0200

    addModelTransformer from string to object

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -7,6 +7,7 @@ use App\Entity\Post;
 use App\Entity\User;
 use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -60,6 +61,18 @@ class PostType extends AbstractType
                     'required' => true] 
                 )
         ;
+
+        $builder->get('date')
+        ->addModelTransformer(new CallbackTransformer(
+            function ($dateToObject) {
+                // transform the string to a DateTime object
+                // return count($rolesArray)? $rolesArray[0]: null;
+            },
+            function ($dateTiString) {
+                // transform the DateTime object back to a string
+                // return $rolesString;
+            }
+        ));
     }
 
     public function configureOptions(OptionsResolver $resolver): void
