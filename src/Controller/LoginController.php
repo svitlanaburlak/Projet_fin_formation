2fcba1c855f0880fd25c400eff707b28a2832aac
Pierre-Henri-Kocan <ph.kocan@gmail.com>
2022-08-23 11:41:02 +0200

    WIP customize error message

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -5,8 +5,6 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
-use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
