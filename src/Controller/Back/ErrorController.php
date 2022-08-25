3e2365010ebe9b5521df6e00c0c0b5e83d35d264
Pierre-Henri-Kocan <ph.kocan@gmail.com>
2022-08-25 14:16:09 +0200

    WIP 404 page

--- src/Controller/Back/ErrorController.php
+++ src/Controller/Back/ErrorController.php
@@ -7,9 +7,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/admin", name="admin_category_")
- */
 
 class ErrorController extends AbstractController
 {
