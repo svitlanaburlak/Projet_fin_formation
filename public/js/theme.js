7ab1a9ac099ad51d4e863f1dc3d50aeabecc27dc
svitlanaburlak <sfetlana.b@gmail.com>
2022-09-01 11:35:35 +0200

    chnage theme button added

--- public/js/theme.js
+++ public/js/theme.js
@@ -3,14 +3,13 @@ const theme = {
 
         const isDarkThemeOn = localStorage.getItem("theme-dark")
 
-        // si la clé n'éxiste pas on la crée avec pour valeur par défaut false
         if(isDarkThemeOn === undefined){
-            console.log("on a pas trouvvé la valeur");
+
             localStorage.setItem("theme-dark", "false");
         }
 
         if(localStorage.getItem("theme-dark") === "true"){
-            console.log("switch");
+
             document.body.classList.toggle("theme-dark");
             const container = document.querySelector(".container-xl");
             container.classList.toggle("bg-dark");
@@ -38,14 +37,9 @@ const theme = {
             card.classList.toggle("bg-dark");
         }
 
-        // Si on était en dark-theme
         if(localStorage.getItem("theme-dark") === 'true'){
-            // la fonction switchTheme nous fais basculer en light
-            // il faut mettre à jour la vaelur de theme-dark dans le localStorage
             localStorage.setItem("theme-dark", false);
         }else{
-            // Si on était en theme light
-            // On doit mettre a jour la valeur de dark-theme dans le localstorage
             localStorage.setItem("theme-dark", true);
         }
     }
