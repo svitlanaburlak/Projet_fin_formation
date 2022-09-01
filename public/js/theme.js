18a30a4021fa63931493a2ee5422e9c96de989e8
svitlanaburlak <sfetlana.b@gmail.com>
2022-09-01 15:40:32 +0200

    theme-dark works on all pages

--- public/js/theme.js
+++ public/js/theme.js
@@ -11,13 +11,52 @@ const theme = {
         if(localStorage.getItem("theme-dark") === "true"){
 
             document.body.classList.toggle("theme-dark");
-            const container = document.querySelector(".container-xl");
-            container.classList.toggle("bg-dark");
 
+            const container = document.querySelector(".container-xl");
+            if(container != undefined)
+            {
+                container.classList.toggle("bg-dark");
+            }
+            
             const cards = document.querySelectorAll(".card");
-        
-            for(const card of cards){
+            if(cards != undefined) 
+            {
+                for(const card of cards){
                 card.classList.toggle("bg-dark");
+                }
+            }
+
+            const table = document.querySelector(".table");
+            if(table != undefined)
+            {
+                table.classList.toggle("theme-dark");
+            }
+
+            const inputs = document.querySelectorAll("input");
+            if(inputs != undefined) 
+            {
+                for(const input of inputs){
+                    input.classList.toggle("bg-dark");
+                    input.classList.toggle("text-warning");
+                }
+            }
+
+            const textareas = document.querySelectorAll("textarea");
+            if(textareas != undefined) 
+            {
+                for(const textarea of textareas){
+                    textarea.classList.toggle("bg-dark");
+                    textarea.classList.toggle("text-warning");
+                }
+            }
+
+            const selects = document.querySelectorAll("select");
+            if(selects != undefined) 
+            {
+                for(const select of selects){
+                    select.classList.toggle("bg-dark");
+                    select.classList.toggle("text-warning");
+                }
             }
         }
 
@@ -29,12 +68,51 @@ const theme = {
 
         document.body.classList.toggle("theme-dark");
         const container = document.querySelector(".container-xl");
-        container.classList.toggle("bg-dark");
+        if(container != undefined)
+        {
+            container.classList.toggle("bg-dark");
+        }
 
         const cards = document.querySelectorAll(".card");
         
-        for(const card of cards){
-            card.classList.toggle("bg-dark");
+        if(cards != undefined) 
+        {
+            for(const card of cards){
+                card.classList.toggle("bg-dark");
+            }
+        }
+
+        const table = document.querySelector(".table");
+        if(table != undefined)
+        {
+            table.classList.toggle("theme-dark");
+        }
+
+        const inputs = document.querySelectorAll("input");
+        if(inputs != undefined) 
+        {
+            for(const input of inputs){
+                input.classList.toggle("bg-dark");
+                input.classList.toggle("text-warning");
+            }
+        }
+
+        const textareas = document.querySelectorAll("textarea");
+        if(textareas != undefined) 
+        {
+            for(const textarea of textareas){
+                textarea.classList.toggle("bg-dark");
+                textarea.classList.toggle("text-warning");
+            }
+        }
+
+        const selects = document.querySelectorAll("select");
+        if(selects != undefined) 
+        {
+            for(const select of selects){
+                select.classList.toggle("bg-dark");
+                select.classList.toggle("text-warning");
+            }
         }
 
         if(localStorage.getItem("theme-dark") === 'true'){
